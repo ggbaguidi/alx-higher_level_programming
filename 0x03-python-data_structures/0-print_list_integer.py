c8957cae818f8c526ca1160352518ee58650b4ba
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-06-09 13:24:45 -0700

    task number 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Args:
+        my_list: a list
+    Return:
+        None
+    """
+    for i in my_list:
+        print("{}".format(i))
