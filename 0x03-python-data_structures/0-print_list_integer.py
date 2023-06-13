6d6d52cb21efb3358a235f6910fcd3055b58814e
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-06-12 17:13:04 -0700

    my project 0x03 for data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    """
-    Args:
-        my_list: a list
-    Return:
-        None
-    """
-    for i in my_list:
-        print("{}".format(i))
+    for list in my_list:
+        print('{:d}'.format(list))
