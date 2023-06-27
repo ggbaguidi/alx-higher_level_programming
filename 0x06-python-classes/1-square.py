c665aec3c323360e93ddc735090a056f9058bcff
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-06-27 15:35:43 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define a square class"""
+
+
+class Square:
+    """class Square that defines a square by: (based on 0-square.py)"""
+
+    def __int__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
