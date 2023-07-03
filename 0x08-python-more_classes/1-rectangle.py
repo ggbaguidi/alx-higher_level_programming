ff190507f13181c800479f28f80aceea2c5dd3c8
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-07-03 12:58:44 -0700

    update a 1-rectangle file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         """to set it"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -37,6 +37,6 @@ class Rectangle:
         """to set it"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
