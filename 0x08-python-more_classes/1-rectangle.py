0f792075812a460206f518df0f75274643e32161
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-07-03 12:42:09 -0700

    update 1.0

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             width (int): ...
             height (int): ...
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
