093018519ad81754e35b8dce987b39224ee22f5c
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-07-10 11:29:12 -0700

    update file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """ A function lookup Return: a list object"""
+    """Return a list of an object's available attributes."""
     return (dir(obj))
