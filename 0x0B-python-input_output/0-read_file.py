fe1c59efa97786472fc22ffb5a50f354dae70862
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-07-11 17:09:50 -0700

    Print the contents of a UTF8 text file to stdout.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """Write a function that reads a text file
     (UTF8) and prints it to stdout"""
 
-    with open(filename, "r") as file:
+    with open(filename, encoding="utf-8") as file:
         print(file.read())
