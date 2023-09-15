7ad2f9e31dfc32c42635ad9bd1982efaf5964a51
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-15 01:53:06 -0700

    feat: add a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that lists all states from database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+
+conn = MySQLdb.connect(host="localhost",
+                       port=3306,
+                       user=mysql_username,
+                       passwd=mysql_password,
+                       db=database_name)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
