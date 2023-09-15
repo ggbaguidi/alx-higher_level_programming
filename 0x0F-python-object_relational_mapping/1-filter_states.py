9639a266910edd46db08c0ee01930d4cbced4fa1
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-15 02:21:51 -0700

    feat: add a new script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=mysql_username,
+                           passwd=mysql_password,
+                           db=database_name)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
