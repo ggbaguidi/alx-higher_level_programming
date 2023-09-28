95145ad5c868237948c4bf4cf4354267cb8dc6e1
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-28 08:05:13 -0700

    refactor: add comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -X GET -sI "$1" | grep Content-Length | awk '{split($0, lenght, ": "); print lenght[2]}'
