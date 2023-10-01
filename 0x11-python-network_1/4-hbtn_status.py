100e8340e7ff84ab17cd0c02f7be894cd4997f44
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-01 11:55:49 -0700

    what's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+What's my status? #1
+"""
+import requests
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url).text
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
